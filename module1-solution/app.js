6519b8547a3bc0de316fd0a6181c248df9e22448
manukv <manukv@synopsys.com>
2016-12-06 19:25:43 +0400

    first module solution

--- module1-solution/app.js
+++ module1-solution/app.js
@@ -8,17 +8,26 @@ angular.module('LunchCheck', [])
 
   function LunchCheckController($scope) {
     $scope.lunchList = "";
+    $scope.messageColor = "";
+    $scope.messageBorder = "";
 
     $scope.lunchCheck = function () {
       var checkingString = $scope.lunchList;
       if (checkingString.split([',']).length > 3) {
         $scope.message = "Too Much";
+        $scope.messageColor = "green";
+        $scope.messageBorder = "2px solid green";
       }
       if (checkingString.split([',']).length <= 3) {
         $scope.message = "Enjoy!";
+        $scope.messageColor = "green";
+        $scope.messageBorder = "2px solid green";
+
       }
       if (checkingString.length == "") {
         $scope.message = "Please enter data first"
+        $scope.messageColor = "red";
+        $scope.messageBorder = "2px solid red";
       }
     };
 
